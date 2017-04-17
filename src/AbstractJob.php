73c68004200c245ac720336c251e92eb0d77edc2
Zero <john.royer@gmail.com>
2017-04-17 09:10:34 +0800

    Implement worker

--- src/AbstractJob.php
+++ src/AbstractJob.php
@@ -4,6 +4,19 @@ namespace JobRunner;
 
 abstract class AbstractJob implements \SplObserver
 {
+    /** @var string unique ID of job */
+    private $jobId = '';
+
     /** @var string run time in 24H format */
     private $runTime = '';
+
+    public function getId()
+    {
+        return $this->jobId;
+    }
+
+    public function getRunTime()
+    {
+        return $this->runTime;
+    }
 }
